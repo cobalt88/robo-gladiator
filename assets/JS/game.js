a20601ff18f16b0ba305cbe4d0be0ef7fef5b89b
cobalt88 <vincentteune@gmail.com>
2022-02-28 21:37:30 -0500

    1.0.1-2.28.22
    updating game.js

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,7 +1,9 @@
 var playerName = window.prompt("What is your robots name?");
 
 var name = "your name";
+
 console.log(name);
+
 console.log("our robots name is " + playerName);
 
 window.alert(playerName);
@@ -11,4 +13,4 @@ function fight() {
   window.alert("The Fight Has Begun!");
 }
 
-// fight();
+fight();
