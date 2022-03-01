11e06d5eaab23fc58ec7351cc7ddde26979d3556
cobalt88 <vincentteune@gmail.com>
2022-02-28 21:28:46 -0500

    1.0.1
    fighting with variables

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,5 +1,14 @@
+var playerName = window.prompt("What is your robots name?");
+
+var name = "your name";
+console.log(name);
+console.log("our robots name is " + playerName);
+
+window.alert(playerName);
+
+
 function fight() {
   window.alert("The Fight Has Begun!");
 }
 
-fight();
+// fight();
