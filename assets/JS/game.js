b70f665a4e1f71ad3e85f4188909012f8c472df9
cobalt88 <vincentteune@gmail.com>
2022-02-28 22:19:05 -0500

    1.0.1
    end of day 2.28.22

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,16 +1,15 @@
 var playerName = window.prompt("What is your robots name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-var name = "your name";
+console.log(playerName, playerAttack, playerHealth)
 
-console.log(name);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("our robots name is " + playerName);
-
-window.alert(playerName);
-
-
-function fight() {
-  window.alert("The Fight Has Begun!");
+var fight = function() {
+  window.alert("welcome to robot gladiators!");
 }
 
 fight();
